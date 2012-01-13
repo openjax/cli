8f8e0291d2a8bd35d21fa2712fcdcd33b15d1750
Seva Safris <seva@safris.org>
2012-01-13 03:33:15 +0000

    Changed the license, updated the whitespacing to 2 spaces, closing #184.

--- src/main/java/org/safris/commons/cli/Options.java
+++ src/main/java/org/safris/commons/cli/Options.java
@@ -1,16 +1,17 @@
-/*  Copyright 2010 Safris Technologies Inc.
+/*  Copyright Safris Software 2008
+ *  
+ *  This code is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.safris.commons.cli;
@@ -135,7 +136,7 @@ public final class Options {
             }
         }
 
-		// Take care of the default values for unspecified options!
+    // Take care of the default values for unspecified options!
         if (argsDefBinding != null) {
             try {
                 for (cli_arguments._option option : argsDefBinding.get_option()) {
@@ -169,7 +170,7 @@ public final class Options {
     }
 
     private Map<String,Option> optionMap = null;
-	private volatile boolean optionMapLock = false;
+  private volatile boolean optionMapLock = false;
     private final Collection<Option> options;
     private final Collection<String> arguments;
     private final org.apache.commons.cli.Options apacheOptions;
@@ -189,21 +190,21 @@ public final class Options {
     }
 
     public Option getOption(String name) {
-		if (optionMapLock)
+    if (optionMapLock)
         	return optionMap.get(name);
 
-		synchronized (options) {
-			if (optionMapLock)
-				return optionMap.get(name);
+    synchronized (options) {
+      if (optionMapLock)
+        return optionMap.get(name);
 
-			optionMap = new HashMap<String,Option>();
-			for (Option option : options)
-				optionMap.put(option.getName(), option);
+      optionMap = new HashMap<String,Option>();
+      for (Option option : options)
+        optionMap.put(option.getName(), option);
 
-			optionMapLock = true;
-		}
+      optionMapLock = true;
+    }
 
-		return optionMap.get(name);
+    return optionMap.get(name);
     }
 
     public String toString() {
--- src/test/java/org/safris/commons/cli/OptionsTest.java
+++ src/test/java/org/safris/commons/cli/OptionsTest.java
@@ -1,16 +1,17 @@
-/*  Copyright 2010 Safris Technologies Inc.
+/*  Copyright Safris Software 2008
+ *  
+ *  This code is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.safris.commons.cli;
@@ -23,33 +24,33 @@ import org.xml.sax.InputSource;
 import static org.junit.Assert.*;
 
 public class OptionsTest {
-    public static void main(String[] args) throws Exception {
-        final OptionsTest optionTest = new OptionsTest();
-        optionTest.testOptions();
-    }
+  public static void main(String[] args) throws Exception {
+    final OptionsTest optionTest = new OptionsTest();
+    optionTest.testOptions();
+  }
 
-    public static void main(Options options) {
-        assertNotNull(options);
-        System.out.println(options.toString());
-        System.out.println("------------------------------------------");
-        for (Option option : options.getOptions())
-            System.out.println("[" + option.getName() + "]: " + option.getValue());
-    }
+  public static void main(Options options) {
+    assertNotNull(options);
+    System.out.println(options.toString());
+    System.out.println("------------------------------------------");
+    for (Option option : options.getOptions())
+      System.out.println("[" + option.getName() + "]: " + option.getValue());
+  }
 
-    @Test
-    public void testOptions() throws Exception {
-        final String[] args = new String[] {
-            "--user", "someuser",
-            "-D", "070919",
-            "--verbose"
-        };
+  @Test
+  public void testOptions() throws Exception {
+    final String[] args = new String[] {
+      "--user", "someuser",
+      "-D", "070919",
+      "--verbose"
+    };
 
-        final cli_arguments arguments = (cli_arguments)Bindings.parse(new InputSource(new FileInputStream("src/test/resources/xml/cli.xml")));
-        final Options options = Options.parse(arguments, args);
-        main(options);
-        assertEquals("user != someuser", "someuser", options.getOption("user").getValue());
-        assertEquals("verbose != true", true, Boolean.parseBoolean(options.getOption("verbose").getValue()));
-        assertEquals("date != 070919", "070919", options.getOption("date").getValue());
-        assertEquals("silent != null", null, options.getOption("silent"));
-    }
+    final cli_arguments arguments = (cli_arguments)Bindings.parse(new InputSource(new FileInputStream("src/test/resources/xml/cli.xml")));
+    final Options options = Options.parse(arguments, args);
+    main(options);
+    assertEquals("user != someuser", "someuser", options.getOption("user").getValue());
+    assertEquals("verbose != true", true, Boolean.parseBoolean(options.getOption("verbose").getValue()));
+    assertEquals("date != 070919", "070919", options.getOption("date").getValue());
+    assertEquals("silent != null", null, options.getOption("silent"));
+  }
 }
