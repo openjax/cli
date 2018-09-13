ed0957bd4d71295cf42f71fe88be33deeebf01c0
Seva Safris <seva@safris.org>
2018-09-13 14:15:18 +0700

    Move cli to easyjax

--- src/test/java/org/easyjax/cli/OptionsTest.java
+++ src/test/java/org/easyjax/cli/OptionsTest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008 lib4j
+/* Copyright (c) 2008 FastJAX
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -14,12 +14,14 @@
  * program. If not, see <http://opensource.org/licenses/MIT/>.
  */
 
-package org.lib4j.cli;
+package org.easyjax.cli;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 
+import org.easyjax.cli.Option;
+import org.easyjax.cli.Options;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
